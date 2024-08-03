af29eb6da4e52b395023a293b12122f8b563775a
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-08-03 22:52:59 +0530

    Delete function

--- intro-to-react/src/App.jsx
+++ intro-to-react/src/App.jsx
@@ -10,12 +10,17 @@ function Counter() {
     setCount(count - 1);
   }
 
+  const deleteCounter = () => {
+    setCount(0);
+  }
+
   return(
     <div className='flex flex-col-1 justify-center'>
     <h1>Counter App</h1>
     <p>Count: {count} </p>
     <button onClick={increment}>Increment</button>
     <button onClick={decrement}>Decrement</button>
+    <button onClick={deleteCounter}>Delete</button>
     </div>
     
   )
